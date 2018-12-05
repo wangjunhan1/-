5015c0d8c0e9eb116aed199579dcaab9c7471971
yankfy <yankfy@qq.com>
2018-12-05 19:23:39 +0800

    add city-search

--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -3,15 +3,17 @@
     <div class="search">
       <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
-    <div class="search-content">
+    <div class="search-content" ref="search" v-show="keyword">
       <ul>
-        <li v-for="item of list">{{item.name}}</li>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有匹配的城市</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
     name: 'CitySearch',
     props: {
@@ -24,10 +26,19 @@ export default {
         timer: null
       }
     },
+    computed: {
+      hasNoData () {
+        return !this.list.length
+      }
+    },
     watch: {
       keyword () {
         if (this.timer) {
           clearTimeout(this.timer)
+        }
+        if (!this.keyword) {
+          this.list = []
+          return
         }
         this.timer = setTimeout(() => {
           const result = []
@@ -38,10 +49,12 @@ export default {
               }
             })
           this.list = result
-          console.log(this.list)
           }
         }, 100)
       }
+    },
+    mounted () {
+      this.scroll = new Bscroll(this.$refs.search)
     }
 }
 </script>
@@ -70,5 +83,10 @@ export default {
     left: 0
     right: 0
     bottom: 0
-    background: green
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      background: #fff
+      color: #666
 </style>
