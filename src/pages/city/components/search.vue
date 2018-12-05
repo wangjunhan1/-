4f835e47b6acdc5a66dc65185f054740969d082b
yankfy <yankfy@qq.com>
2018-12-05 18:41:50 +0800

    add search1

--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -1,12 +1,48 @@
 <template>
+  <div>
     <div class="search">
-      <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
+    <div class="search-content">
+      <ul>
+        <li v-for="item of list">{{item.name}}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+    props: {
+      cities: Object
+    },
+    data () {
+      return {
+        keyword: '',
+        list: [],
+        timer: null
+      }
+    },
+    watch: {
+      keyword () {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const result = []
+          for(let i in this.cities) {
+            this.cities[i].forEach((value) => {
+              if (value.name.indexOf(this.keyword) > -1 || value.spell.indexOf(this.keyword) > -1) {
+                result.push(value)
+              }
+            })
+          this.list = result
+          console.log(this.list)
+          }
+        }, 100)
+      }
+    }
 }
 </script>
 
@@ -26,4 +62,13 @@ export default {
       text-align: center
       border-radius: .06rem
       color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: green
 </style>
